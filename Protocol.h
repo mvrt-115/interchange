56a8fa6ec81ae5e36be7fe73463edb3ebd1f1c01
Marcus Plutowski <achierius@gmail.com>
2016-01-26 13:27:20 -0800

    Adds the purely abstract base class from which server/client pairs should be generated

---
+++ Protocol.h
@@ -0,0 +1,25 @@
+#ifndef _INTERCHANGE_PROTOCOL_H
+#define _INTERCHANGE_PROTOCOL_H
+
+#include "Handler.h"
+#include <Boost/asio.hpp>
+#include <string>
+
+class Handler;
+
+class Protocol{
+public:
+	Protocol(string name, void (*retData) (string), boost::asio::ip::address target, int port);
+	virtual void sendData(string data) = 0;	
+	void setName(string newName);
+	string getName();
+private:
+	void (*retData) (string);
+	boost::asio::io_service io_service;
+	string name;
+	boost::asio::ip::address targetIP;
+	int targetPort;
+};
+
+#endif
+
