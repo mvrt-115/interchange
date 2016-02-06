d64e1729d15be155841dd3fb910adbdc3fbad248
Marcus Plutowski <achierius@gmail.com>
2016-02-05 17:43:57 -0800

    Changes Daemon to use the new Datum type and implements the methods

--- Daemon.h
+++ Daemon.h
@@ -1,19 +1,38 @@
+#ifndef __INTERCHANGE_DAEMON_H
+#define __INTERCHANGE_DAEMON_H
+
 #include <string>
 #include <vector>
 #include "Handler.h"
+#include "Timer.h"
+#include "Datum.h"
 
+/*
+* @author Marcus Plutowski
+*/
 class Daemon{
+friend class Handler;
+
 public:
 	Daemon(std::string uniqueName, Handler* parentHandler, int initRefreshRate); //uniqueName defines the Daemon for the Handler. refreshRate is in ms
-	void sendData(std::string Data, std::string protocolName);
+	void sendData(Datum::Datum Data, std::string protocolName);
+	
 	std::string getData(string protocolName); //Retrieves the last data retrieved by the Daemon from said protocol
 	std::string waitData(string protocolName); //Waits for new data from said protocol
-	void setRefreshRate(int newRefreshRate);
-	int getRefreshRate();
+
+	void setRefreshRate(unsigned int newRefreshRate);
+	unsigned int getRefreshRate();
 private:
 	void pullData();
-	std::map<std::string, std::string> lastRecieved;
-	Handler* handler;
 	
+	std::map<std::string, Datum::Datum> lastRecieved;
+	Handler* handler;
+	std::string name;
+	
+	unsigned int refreshRate;
+	
+	enum class ReadStatus = {Immediate, Last, Distant}; 
+	std::map<std::string, ReadStatus> readStatus;
 };
 
+#endif //__INTERCHANGE_DAEMON_H
