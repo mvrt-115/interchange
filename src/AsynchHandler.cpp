a278005b9b70920d98f7c2ab310bb698350b7641
Marcus Plutowski <achierius@gmail.com>
2016-02-18 02:40:18 -0800

    Reformats AsynchHandler.h

--- src/AsynchHandler.cpp
+++ src/AsynchHandler.cpp
@@ -4,48 +4,55 @@
 /*
 *@author Marcus Plutowski
 */
-AsynchHandler::AsynchHandler(IPAddr targetAddress, Timer* timer){
-	
+AsynchHandler::AsynchHandler(IPAddr targetAddress, Timer* timer)
+{
 }
-AsynchHandler::~AsynchHandler(){
-
-}
-
-void AsynchHandler::addDaemon(Daemon* newDaemon) override{
-	Daemons.insert( std::pair< std::string, Daemon* >(newDaemon->getName(), newDaemon);
-}
-void AsynchHandler::removeDaemon(std::string name) override{
-	Daemons.erase(name);
+AsynchHandler::~AsynchHandler()
+{
 }
 
-void AsynchHandler::addProtocol(Protocol* newProtocol) override{
-	Protocols.insert( std::pair< std::string, Protocol* >(newProtocol->getName(), newProtocol);
+void AsynchHandler::addDaemon(Daemon* newDaemon) override
+{
+        Daemons.insert( std::pair< std::string, Daemon* >(newDaemon->getName(), newDaemon);
 }
-void AsynchHandler::removeProtocol(std::string name) override{
-	Daemons.erase(name);
-}
-
-void AsynchHandler::stageData(Datum data, std::string protocolName){
-
-}
-void AsynchHandler::retData(Datum data, std::string protocolName){
-
+void AsynchHandler::removeDaemon(std::string name) override
+{
+    Daemons.erase(name);
 }
 
-void AsynchHandler::regulateHandler(){
-	while(true){
-		this->tickDaemons();
-		this->tickProtocols();
-	}
+void AsynchHandler::addProtocol(Protocol* newProtocol) override
+{
+        Protocols.insert( std::pair< std::string, Protocol* >(newProtocol->getName(), newProtocol);
 }
-void AsynchHandler::tickDaemons(){
-	for(Daemon* daemon : Daemons){
-		daemon->pullData();
-		/* TODO FINISH REGULATION */
-	} 
+void AsynchHandler::removeProtocol(std::string name) override
+{
+    Daemons.erase(name);
 }
-void AsynchHandler::tickProtocols(){
-	for(Protocol* protocol : Protocol){
-		/* TODO IMPLEMENT Protocol::fetchData() & FINISH REGULATION */
-	}
+
+void AsynchHandler::stageData(Datum data, std::string protocolName)
+{
+}
+void AsynchHandler::retData(Datum data, std::string protocolName)
+{
+}
+
+void AsynchHandler::regulateHandler()
+{
+    while (true) {
+        this->tickDaemons();
+        this->tickProtocols();
+    }
+}
+void AsynchHandler::tickDaemons()
+{
+    for (Daemon* daemon : Daemons) {
+        daemon->pullData();
+        /* TODO FINISH REGULATION */
+    }
+}
+void AsynchHandler::tickProtocols()
+{
+    for (Protocol* protocol : Protocol) {
+        /* TODO IMPLEMENT Protocol::fetchData() & FINISH REGULATION */
+    }
 }
