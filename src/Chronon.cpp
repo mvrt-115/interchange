d75bfb3bf5cb3dbd470e3cd45d9bc2462932ad1e
BenCuan <Ben.Cuan@gmail.com>
2016-02-17 18:31:02 -0800

    Added chronon code

---
+++ src/Chronon.cpp
@@ -0,0 +1,22 @@
+#include "Chronon.h"
+
+Chronon::Chronon(short mantissa, short exponent)
+{
+}
+
+short Chronon::getMantissa(Chronon &a)
+{
+	return a.mantissa;
+}
+
+short Chronon::getExponent(Chronon &a)
+{
+	return a.exponent;
+}
+
+void Chronon::changeExponent(Chronon &a, short newExponent)
+{
+	for (short e = a.exponent; e > 0; e--) a.mantissa *= a.mantissa;
+	for (short e = newExponent; e > 0; e--) a.mantissa /= a.mantissa;
+
+}
