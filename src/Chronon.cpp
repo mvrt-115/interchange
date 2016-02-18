3a52c4d5938b3806b0cbcc4d55acf6defa04e066
BenCuan <Ben.Cuan@gmail.com>
2016-02-18 14:58:57 -0800

    Formatted code better

--- src/Chronon.cpp
+++ src/Chronon.cpp
@@ -2,21 +2,21 @@
 
 Chronon::Chronon(short mantissa, short exponent)
 {
+        //TODO
 }
 
-short Chronon::getMantissa(Chronon &a)
+short Chronon::getMantissa (Chronon &a)
 {
-	return a.mantissa;
+        return a.mantissa;
 }
 
-short Chronon::getExponent(Chronon &a)
+short Chronon::getExponent (Chronon &a)
 {
-	return a.exponent;
+        return a.exponent;
 }
 
-void Chronon::changeExponent(Chronon &a, short newExponent)
+void Chronon::changeExponent (Chronon &a, short newExponent)
 {
-	for (short e = a.exponent; e > 0; e--) a.mantissa *= a.mantissa;
-	for (short e = newExponent; e > 0; e--) a.mantissa /= a.mantissa;
-
+        for (short e = a.exponent; e > 0; e--) a.mantissa *= a.mantissa;
+        for (short e = newExponent; e > 0; e--) a.mantissa /= a.mantissa;
 }
